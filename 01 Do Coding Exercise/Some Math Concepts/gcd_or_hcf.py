f2c6fb933a30821df7843d1dd8fa18845d1a1f3b
sohanxsahoo <116956354+sohanxsahoo@users.noreply.github.com>
2024-12-13 19:55:47 +0530

    update process in gcd

--- 01 Do Coding Exercise/Some Math Concepts/gcd_or_hcf.py
+++ 01 Do Coding Exercise/Some Math Concepts/gcd_or_hcf.py
@@ -1,11 +1,29 @@
-num1 = int(input("Enter the number: "))
-num2 = int(input("Enter the number: "))
+# num1 = int(input("Enter the number: "))
+# num2 = int(input("Enter the number: "))
 
-smaller = min(num1, num2)
-gcd = 1
+# smaller = min(num1, num2)
+# gcd = 1
 
-for i in range(1, smaller + 1):
-    if num1 % i == 0 and num2 % i == 0:
-        gcd = i
+# for i in range(1, smaller + 1):
+#     if num1 % i == 0 and num2 % i == 0:
+#         gcd = i
 
-print(f"The GCD of {num1} and {num2} is {gcd}.")
+# print(f"The GCD of {num1} and {num2} is {gcd}.")
+
+
+'''or'''
+
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+while a > 0 and b > 0:
+    if a > b:
+        a = a % b
+    else:
+        b = b % a
+
+if a == 0:
+    print(f"The GCD is {b}")
+else:
+    print(f"The GCD is {a}")
