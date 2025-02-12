d6fa78571418c46ea93a620b670d5a49e1ed9cef
sohanxsahoo <sohanxsahoo@gmail.com>
2025-02-12 16:48:21 +0530

    dir name changes

--- 01 Do Coding Exercise/DSA Topic Wise Probs/merge.py
+++ 01 Do Coding Exercise/DSA Topic Wise Probs/merge.py
@@ -14,7 +14,7 @@ def merge_python_files(source_folder, output_file):
                     outfile.write("\n\n")
 
 
-source_folder = r"C:\Users\sohan\Desktop\Start Over\Gain Knowledge\Software Engineering\01 Programming Tech Stack\Python Programming\01 Do Coding Exercise\DSA Class Assignment\Singly Linked Lists Python"
+source_folder = r"C:\Users\sohan\Desktop\Start Over\Gain Knowledge\Software Engineering\01 Programming Tech Stack\Python Programming\01 Do Coding Exercise\DSA Topic Wise Probs\Singly Linked Lists Python"
 output_file = "merged_assignment.py"
 
 merge_python_files(source_folder, output_file)
